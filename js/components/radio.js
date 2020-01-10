ddcac7576c821efb83c1fa44d7787b39453303b1
Jun <jun@advmeds.com>
2020-01-10 17:21:22 +0800

    榨菜肉絲麵

--- js/components/radio.js
+++ js/components/radio.js
@@ -29,7 +29,8 @@ export default Vue.component('radio', {
         item.Binding.forEach(Guid => {
           eventBus.$emit('connect', { Guid, status: true });
         });
-        return false; // 增加這行遇到的原因：兩個選項綁定同個題目時，跳題效果就會消失，詳細原因有待深入研究。
+        // return false; // 增加這行遇到的原因：兩個選項綁定同個題目時，跳題效果就會消失，詳細原因有待深入研究。
+        return;
       }
       data.Options.forEach(option => {
         if (option.Binding.length > 0 && option.Guid !== item.Guid) {
